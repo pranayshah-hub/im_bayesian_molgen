4ff4c811dbd598c70d9d8d2a707742cd2da1b12c
Pranay Shah <pranay96shah@gmail.com>
2021-10-21 19:23:30 +0100

    Add ICD calculations

--- read_events.py
+++ read_events.py
@@ -52,6 +52,79 @@ import operator
 # #             ["/data/PycharmProjects/B2Q_code/bdqn2/events.out.tfevents.1618617464.beilun.doc.ic.ac.uk",
 # #              ])
 
+def get_icd_per_trial(folder, top_k=5000):
+    file_to_molecules = dict()
+
+    molecule_to_reward = dict()
+
+    file_names = os.listdir(folder)
+    file_names = [file_name for file_name in file_names if "molecule_output" in file_name]
+    for file_name in file_names:
+        file_path = folder + "/" + file_name
+
+        molecule_to_reward[file_name] = dict()
+
+        with open(file_path, "r") as fp:
+            for row in fp:
+                clean_row = row.strip().split("\t")
+
+                molecule_to_reward[file_name][clean_row[0]] = float(clean_row[1])
+
+        molecules_sorted = sorted(molecule_to_reward[file_name], key=molecule_to_reward[file_name].get)
+
+        file_to_molecules[file_name] = molecules_sorted
+
+    icd_list_all = list()
+    for file_name in file_names:
+        maxes = list()
+        reward_list = list()
+        smiles_list = list()
+        for k in range(top_k):
+            m_id = - top_k + k
+            m = file_to_molecules[file_name][m_id]
+            reward_list.append(molecule_to_reward[file_name][m])
+            smiles_list.append(m)
+            if len(reward_list) == 1:
+                max_smiles = smiles_list[0]
+            elif len(reward_list) > 1:
+                max_smiles = smiles_list[reward_list.index(max(reward_list))]
+            maxes.append(max_smiles)
+
+        smiles = [[mol1, mol2] for i, mol1 in enumerate(maxes) for j, mol2 in enumerate(maxes) if i < j]
+
+        icd_list = list()
+        for pair in smiles:
+            sim = get_properties(pair[0], target_molecule=pair[1])
+            tan_d = 1 - sim
+            icd_list.append(tan_d)
+
+        div = np.mean(icd_list)
+
+        icd_list_all.append(div)
+
+    print(icd_list_all)
+    print()
+    print(np.mean(icd_list_all))
+
+    return icd_list_all, np.mean(icd_list_all)
+
+def write_diversities(folder, top_k=50):
+    for subdir, dirs, _ in os.walk(folder):
+        for d in dirs:
+            path = folder + d
+            for f in os.listdir(path):
+                if f.endswith('.txt'):
+                    mean_diversity_per_trial, mean_diversity_per_exp = get_icd_per_trial(path, top_k=top_k)
+                    if not os.path.exists(folder + '/' + 'diversity_' + str(top_k) + '.txt'):
+                        with open(folder + '/' + 'diversity_' + str(top_k) + '.txt', 'w') as f:
+                            f.write(d + ': \n' + str(mean_diversity_per_trial) + '\n' + str(mean_diversity_per_exp) + '\n\n')
+                    else:
+                        with open(folder + '/' + 'diversity_' + str(top_k) + '.txt', 'a') as f:
+                            f.write(d + ': \n' + str(mean_diversity_per_trial) + '\n' + str(mean_diversity_per_exp) + '\n\n')
+                    break
+            else:
+                continue
+
 
 def summarise_molecule_output(folder,
                               top_k=50):
